3a6d69f29c035c2fc0824cfc24cb830165f9f646
Tishko1 <113176690+Tishko1@users.noreply.github.com>
2023-02-13 17:07:01 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,11 +13,11 @@ export class App extends Component {
   };
 
   componentDidMount() {
-    const contacts = localStorage.getItem('contacts');
+    const contacts = localStorage.getItem('contacts') ?? [];
     const parsedContacts = JSON.parse(contacts);
 
     if (parsedContacts) {
-      this.setState({ contacts: parsedContacts });
+      this.setState({ contacts: parsedContacts  });
     }
   }
 
